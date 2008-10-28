20fcb6fafb033deefa320cb4877fcd93d67a1ac1
binarylogic <bjohnson@binarylogic.com>
2008-10-27 22:37:51 -0400

    Bumped to Sha512

--- lib/authgasm.rb
+++ lib/authgasm.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + "/authgasm/version"
 
 require File.dirname(__FILE__) + "/authgasm/controller_adapters/rails_adapter" if defined?(Rails)
 
-require File.dirname(__FILE__) + "/authgasm/sha256_crypto_provider"
+require File.dirname(__FILE__) + "/authgasm/sha512_crypto_provider"
 require File.dirname(__FILE__) + "/authgasm/acts_as_authentic"
 require File.dirname(__FILE__) + "/authgasm/session/active_record_trickery"
 require File.dirname(__FILE__) + "/authgasm/session/callbacks"
--- lib/authgasm/sha512_crypto_provider.rb
+++ lib/authgasm/sha512_crypto_provider.rb
@@ -1,13 +1,13 @@
 module Authgasm
-  # = Sha256 Crypto Provider
+  # = Sha512 Crypto Provider
   #
   # The acts_as_authentic method allows you to pass a :crypto_provider option. This allows you to use any type of encryption you like. Just create a class with a class level encrypt and decrypt method.
   # The password will be passed as the single parameter to each of these methods so you can do your magic.
   #
   # If you are encrypting via a hash just don't include a decrypt method, since hashes can't be decrypted. Authgasm will notice this adjust accordingly.
-  class Sha256CryptoProvider
+  class Sha512CryptoProvider
     def self.encrypt(pass)
-      Digest::SHA256.hexdigest(pass)
+      Digest::SHA512.hexdigest(pass)
     end
   end
 end
