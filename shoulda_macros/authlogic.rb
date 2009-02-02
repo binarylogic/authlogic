4b3efa4d50130838266d0e3fc405f851314bfdbb
binarylogic <bjohnson@binarylogic.com>
2009-02-02 13:48:23 -0500

    Change shoulda macros implementation to follow the shoulda documentation

--- shoulda_macros/authlogic.rb
+++ shoulda_macros/authlogic.rb
@@ -1,14 +1,12 @@
-require "test/unit"
-
 module Authlogic
   module ShouldaMacros
-    def should_be_authentic
-      klass = model_class
-      should "acts as authentic" do
-        assert klass.respond_to?(:acts_as_authentic_config)
+    class Test::Unit::TestCase
+      def self.should_be_authentic
+        klass = model_class
+        should "acts as authentic" do
+          assert klass.respond_to?(:acts_as_authentic_config)
+        end
       end
     end
   end
 end
-
-Test::Unit::TestCase.extend Authlogic::ShouldaMacros
