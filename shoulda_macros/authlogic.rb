9700ca908f6c1291c526a452bdad04a81c270fa5
hardbap <hardbap@gmail.com>
2009-04-13 08:55:04 +0800

    need to test for :password= and :valid_password? on an instance.
    Signed-off-by: Ben Johnson <bjohnson@binarylogic.com>

--- shoulda_macros/authlogic.rb
+++ shoulda_macros/authlogic.rb
@@ -4,8 +4,8 @@ module Authlogic
       def self.should_be_authentic
         klass = model_class
         should "acts as authentic" do
-          assert klass.respond_to?(:password=)
-          assert klass.respond_to?(:valid_password?)
+          assert klass.new.respond_to?(:password=)
+          assert klass.new.respond_to?(:valid_password?)
         end
       end
     end
