70e8a05593b9e942e356f833222796661d4b5d02
Ricardo Shiota Yasuda <shadow11@gmail.com>
2009-07-22 03:48:46 +0800

    Changed model_class to described_type for compatibility with Shoulda 2.10.2
    Signed-off-by: Ben Johnson <bjohnson@binarylogic.com>

--- shoulda_macros/authlogic.rb
+++ shoulda_macros/authlogic.rb
@@ -2,7 +2,7 @@ module Authlogic
   module ShouldaMacros
     class Test::Unit::TestCase
       def self.should_be_authentic
-        klass = model_class
+        klass = described_type rescue model_class
         should "acts as authentic" do
           assert klass.new.respond_to?(:password=)
           assert klass.new.respond_to?(:valid_password?)
