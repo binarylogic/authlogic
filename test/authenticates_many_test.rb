3900c94d2018d7b016344f030b2eb1cd222b6491
Victor Pereira <victor.pereira@adyton.net>
2011-02-22 14:33:55 +0100

    working on authenticates_many bug

--- test/authenticates_many_test.rb
+++ test/authenticates_many_test.rb
@@ -1,16 +1,30 @@
 require 'test_helper'
 
 class AuthenticatesManyTest < ActiveSupport::TestCase
+  
+  def setup
+    @zack = users(:zack)
+    @ben = users(:ben)
+    @binary_logic = companies(:binary_logic)
+    
+  end
+  
+  
+  def test_warmup
+    assert @zack
+    assert @ben
+    assert @binary_logic
+  end
+  
   def test_scoping
-    zack = users(:zack)
-    ben = users(:ben)
-    binary_logic = companies(:binary_logic)
-    set_session_for(zack)
     
-    assert !binary_logic.user_sessions.find
+    #puts @zack.inspect
+    #set_session_for(@zack)
     
-    set_session_for(ben)
+    puts @binary_logic.user_sessions.inspect
+    assert !@binary_logic.user_sessions.find
     
-    assert binary_logic.user_sessions.find
+    #set_session_for(@ben)
+    assert @binary_logic.user_sessions.find
   end
 end
