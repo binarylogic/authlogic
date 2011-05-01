d22a5ed2e0c83e5e1b45511344b04e1a1ec6e7b2
Binary Logic <bjohnson@binarylogic.com>
2011-04-30 20:18:14 -0400

    user where

--- lib/authlogic/session/scopes.rb
+++ lib/authlogic/session/scopes.rb
@@ -12,14 +12,14 @@ module Authlogic
           attr_writer :scope
         end
       end
-      
+
       # = Scopes
       module ClassMethods
         # The current scope set, should be used in the block passed to with_scope.
         def scope
           Thread.current[:authlogic_scope]
         end
-        
+
         # What with_scopes focuses on is scoping the query when finding the object and the name of the cookie / session. It works very similar to
         # ActiveRecord::Base#with_scopes. It accepts a hash with any of the following options:
         #
@@ -34,11 +34,11 @@ module Authlogic
         #
         # Eseentially what the above does is scope the searching of the object with the sql you provided. So instead of:
         #
-        #   User.find(:first, :conditions => "login = 'ben'")
+        #   User.where("login = 'ben'").first
         #
         # it would be:
         #
-        #   User.find(:first, :conditions => "login = 'ben' and account_id = 2")
+        #   User.where("login = 'ben' and account_id = 2").first
         #
         # You will also notice the :id option. This works just like the id method. It scopes your cookies. So the name of your cookie will be:
         #
@@ -65,31 +65,31 @@ module Authlogic
           self.scope = nil
           result
         end
-        
+
         private
           def scope=(value)
             Thread.current[:authlogic_scope] = value
           end
       end
-      
+
       module InstanceMethods
         # Setting the scope if it exists upon instantiation.
         def initialize(*args)
           self.scope = self.class.scope
           super
         end
-        
+
         # The scope of the current object
         def scope
           @scope ||= {}
         end
-        
+
         private
           # Used for things like cookie_key, session_key, etc.
           def build_key(last_part)
             [scope[:id], super].compact.join("_")
           end
-          
+
           def search_for_record(*args)
             klass.send(:with_scope, :find => (scope[:find_options] || {})) do
               klass.send(*args)
