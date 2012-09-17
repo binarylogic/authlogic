d60f81bbd6565e879db5f6c286147bf46c514dcd
Ben Somers <somers.ben@gmail.com>
2012-09-17 16:16:11 -0700

    Adding SCrypt support
    Modeled very closely on BCrypt support. Unfortunately,
    there is no simple way to detect tuning changes
    (like BCrypt does with its cost parameter) with the
    current ruby implementation of SCrypt.

---
+++ test/crypto_provider_test/scrypt_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+module CryptoProviderTest
+  class SCryptTest < ActiveSupport::TestCase
+    def test_encrypt
+      assert Authlogic::CryptoProviders::SCrypt.encrypt("mypass")
+    end
+    
+    def test_matches
+      hash = Authlogic::CryptoProviders::SCrypt.encrypt("mypass")
+      assert Authlogic::CryptoProviders::SCrypt.matches?(hash, "mypass")
+    end
+  end
+end
