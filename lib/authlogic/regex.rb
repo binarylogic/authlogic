49aec8436c5697483fcc49e9f8f1098d602344fe
Jo Cranford <jo.cranford@gmail.com>
2014-03-30 12:03:55 +1100

    Update email regex to not care about case so that it can work with Judge validations

--- lib/authlogic/regex.rb
+++ lib/authlogic/regex.rb
@@ -11,9 +11,9 @@ module Authlogic
     # for regular expressions.
     def self.email
       @email_regex ||= begin
-        email_name_regex  = '[A-Z0-9_\.%\+\-\']+'
-        domain_head_regex = '(?:[A-Z0-9\-]+\.)+'
-        domain_tld_regex  = '(?:[A-Z]{2,4}|museum|travel|онлайн)'
+        email_name_regex  = '[a-zA-Z0-9_\.%\+\-\']+'
+        domain_head_regex = '(?:[a-zA-Z0-9\-]+\.)+'
+        domain_tld_regex  = '(?:[a-zA-Z]{2,4}|museum|travel|онлайн)'
         /\A#{email_name_regex}@#{domain_head_regex}#{domain_tld_regex}\z/i
       end
     end
