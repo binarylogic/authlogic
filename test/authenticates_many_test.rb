6253c419bf4506c2b5db8997f121af7ce91e91b7
zhaowenlan1779 <zhaowenlan1779@sina.com>
2017-09-23 10:32:16 +0800

    trailing whitespace

--- test/authenticates_many_test.rb
+++ test/authenticates_many_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class AuthenticatesManyTest < ActiveSupport::TestCase
-  
+
   def set_company_specific_session_for(company, user)
     id = company.id
     controller.session["company_#{id}_user_credentials"] = user.persistence_token
     controller.session["company_#{id}_user_credentials_id"] = user.id
   end
-  
+
   def test_scoping
     zack = users(:zack)
     ben = users(:ben)
