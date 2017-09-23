15228186ecaac228a5f3f53c0aba298a9fc22a98
zhaowenlan1779 <zhaowenlan1779@sina.com>
2017-09-23 10:27:26 +0800

    update authenticates_many test

--- test/authenticates_many_test.rb
+++ test/authenticates_many_test.rb
@@ -1,15 +1,22 @@
 require 'test_helper'
 
 class AuthenticatesManyTest < ActiveSupport::TestCase
+  
+  def set_company_specific_session_for(company,user)
+    id = company.id
+    controller.session["company_#{id}_user_credentials"] = user.persistence_token
+    controller.session["company_#{id}_user_credentials_id"] = user.id
+  end
+  
   def test_scoping
     zack = users(:zack)
     ben = users(:ben)
     binary_logic = companies(:binary_logic)
-    set_session_for(zack)
+    set_company_specific_session_for(binary_logic,zack)
 
     refute binary_logic.user_sessions.find
 
-    set_session_for(ben)
+    set_company_specific_session_for(binary_logic,ben)
 
     assert binary_logic.user_sessions.find
   end
