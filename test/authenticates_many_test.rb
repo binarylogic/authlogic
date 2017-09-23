bd36c7d8ced48c0c8fc47edb0dc811d8d9d70716
zhaowenlan1779 <zhaowenlan1779@sina.com>
2017-09-23 10:30:05 +0800

    robocop style error

--- test/authenticates_many_test.rb
+++ test/authenticates_many_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class AuthenticatesManyTest < ActiveSupport::TestCase
   
-  def set_company_specific_session_for(company,user)
+  def set_company_specific_session_for(company, user)
     id = company.id
     controller.session["company_#{id}_user_credentials"] = user.persistence_token
     controller.session["company_#{id}_user_credentials_id"] = user.id
@@ -12,11 +12,11 @@ class AuthenticatesManyTest < ActiveSupport::TestCase
     zack = users(:zack)
     ben = users(:ben)
     binary_logic = companies(:binary_logic)
-    set_company_specific_session_for(binary_logic,zack)
+    set_company_specific_session_for(binary_logic, zack)
 
     refute binary_logic.user_sessions.find
 
-    set_company_specific_session_for(binary_logic,ben)
+    set_company_specific_session_for(binary_logic, ben)
 
     assert binary_logic.user_sessions.find
   end
