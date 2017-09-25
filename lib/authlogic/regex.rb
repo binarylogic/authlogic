af824e0d23da6dd92b6145cbb92597c2e7a5f8a9
John Schmidt <jschmidt@aerserv.com>
2017-09-25 14:51:23 -0700

    Allow max length of tld to 24 https://data.iana.org/TLD/tlds-alpha-by-domain.txt

--- lib/authlogic/regex.rb
+++ lib/authlogic/regex.rb
@@ -14,7 +14,7 @@ module Authlogic
       @email_regex ||= begin
         email_name_regex  = '[A-Z0-9_\.&%\+\-\']+'
         domain_head_regex = '(?:[A-Z0-9\-]+\.)+'
-        domain_tld_regex  = '(?:[A-Z]{2,13})'
+        domain_tld_regex  = '(?:[A-Z]{2,25})'
         /\A#{email_name_regex}@#{domain_head_regex}#{domain_tld_regex}\z/i
       end
     end
