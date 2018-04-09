edac0a2cd052f42ea150fe4be486f668303f2439
Jared Beck <jared@jaredbeck.com>
2018-04-09 01:56:09 -0400

    Deprecate crypto_providers/wordpress.rb
    See reasons given in comments and in deprecation warning message.

--- lib/authlogic/crypto_providers/wordpress.rb
+++ lib/authlogic/crypto_providers/wordpress.rb
@@ -1,6 +1,35 @@
 require 'digest/md5'
+
+::ActiveSupport::Deprecation.warn(
+  <<-EOS,
+    authlogic/crypto_providers/wordpress.rb is deprecated without replacement.
+    Yes, the entire file. Don't `require` it. Let us know ASAP if you are still
+    using it.
+
+    Reasons for deprecation: This file is not autoloaded by
+    `authlogic/crypto_providers.rb`. It's not documented. There are no tests.
+    So, it's likely used by a *very* small number of people, if any. It's never
+    had any contributions except by its original author, Jeffry Degrande, in
+    2009. It is unclear why it should live in the main authlogic codebase. It
+    could be in a separate gem, authlogic-wordpress, or it could just live in
+    Jeffry's codebase, if he still even needs it, in 2018, nine years later.
+  EOS
+  caller(1)
+)
+
 module Authlogic
   module CryptoProviders
+    # Crypto provider to transition from wordpress user accounts. Written by
+    # Jeffry Degrande in 2009. First released in 2.1.3.
+    #
+    # Problems:
+    #
+    # - There are no tests.
+    # - We can't even figure out how to run this without it crashing.
+    # - Presumably it implements some spec, but it doesn't mention which.
+    # - It is not documented anywhere.
+    # - There is no PR associated with this, and no discussion about it could be found.
+    #
     class Wordpress
       class << self
         ITOA64 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.freeze
