76e07e858fdf7a2084645c77c403d24f9807c2da
Jared Beck <jared@jaredbeck.com>
2018-09-18 02:07:19 -0400

    Explain PriorityRecord better
    [ci skip]

--- lib/authlogic/session/priority_record.rb
+++ lib/authlogic/session/priority_record.rb
@@ -2,12 +2,46 @@
 
 module Authlogic
   module Session
-    # The point of this module is to avoid the StaleObjectError raised when
-    # lock_version is implemented in ActiveRecord. We accomplish this by using a
-    # "priority record". Meaning this record is used if possible, it gets
-    # priority. This way we don't save a record behind the scenes thus making an
-    # object being used stale.
+    # This module supports ActiveRecord's optimistic locking feature, which is
+    # automatically enabled when a table has a `lock_version` column.
+    #
+    # ```
+    # # https://api.rubyonrails.org/classes/ActiveRecord/Locking/Optimistic.html
+    # p1 = Person.find(1)
+    # p2 = Person.find(1)
+    # p1.first_name = "Michael"
+    # p1.save
+    # p2.first_name = "should fail"
+    # p2.save # Raises an ActiveRecord::StaleObjectError
+    # ```
+    #
+    # Now, consider the following Authlogic scenario:
+    #
+    # ```
+    # User.log_in_after_password_change = true
+    # ben = User.find(1)
+    # UserSession.create(ben)
+    # ben.password = "newpasswd"
+    # ben.password_confirmation = "newpasswd"
+    # ben.save
+    # ```
+    #
+    # We've used one of Authlogic's session maintenance features,
+    # `log_in_after_password_change`. So, when we call `ben.save`, there is a
+    # `before_save` callback that logs Ben in (`UserSession.find`). Well, when
+    # we log Ben in, we update his user record, eg. `login_count`. When we're
+    # done logging Ben in, then the normal `ben.save` happens. So, there were
+    # two `update` queries. If those two updates came from different User
+    # instances, we would get a `StaleObjectError`.
+    #
+    # Our solution is to carefully pass around a single `User` instance, using
+    # it for all `update` queries, thus avoiding the `StaleObjectError`.
+    #
+    # TODO: Perhaps this file should be merged into `session/persistence.rb`
+    #
+    # @api private
     module PriorityRecord
+      # @api private
       def self.included(klass)
         klass.class_eval do
           attr_accessor :priority_record
@@ -18,6 +52,8 @@ module Authlogic
       # is through an array:
       #
       #   session.credentials = [real_user_object, priority_user_object]
+      #
+      # @api private
       def credentials=(value)
         super
         values = value.is_a?(Array) ? value : [value]
@@ -26,11 +62,13 @@ module Authlogic
 
       private
 
+      # @api private
       def attempted_record=(value)
         value = priority_record if value == priority_record
         super
       end
 
+      # @api private
       def save_record(alternate_record = nil)
         r = alternate_record || record
         super if r != priority_record
