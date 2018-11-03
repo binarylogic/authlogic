4966802baf516f77aaf4bec55fbae812e4b13155
Jared Beck <jared@jaredbeck.com>
2018-11-02 20:15:45 -0400

    Rename gemfiles
    By naming the gemfiles with a .rb extension, we automatically
    get syntax highlighting, but more importantly, we get linting.

--- gemfiles/Gemfile.rails-6.0.x
+++
@@ -1,7 +0,0 @@
-source "https://rubygems.org"
-gemspec :path => ".."
-
-# TODO: Use actual version number
-gem "activerecord", github: 'rails/rails'
-gem "activesupport", github: 'rails/rails'
-gem 'sqlite3', :platforms => :ruby
---
+++ gemfiles/rails-6.0.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+gemspec path: ".."
+
+# TODO: Use actual version number
+gem "activerecord", github: "rails/rails"
+gem "activesupport", github: "rails/rails"
+gem "sqlite3", platforms: :ruby
