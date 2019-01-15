0c736048ca57565b5284e94426f3dd3da8dcfd40
Anton K <iggant@gmail.com>
2019-01-16 01:51:10 +0200

    Fix Rails case_insensitive_comparison change API

--- gemfiles/rails_6.0.rb
+++ gemfiles/rails_6.0.rb
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 gemspec path: ".."
 
 # TODO: Use actual version number
+gem "activemodel", github: "rails/rails"
 gem "activerecord", github: "rails/rails"
 gem "activesupport", github: "rails/rails"
 gem "sqlite3", platforms: :ruby
