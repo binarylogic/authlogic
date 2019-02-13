f6a58165a587270fb06adde10acb95c42fc4d2b8
Jared Beck <jared@jaredbeck.com>
2019-02-13 12:25:11 -0500

    Tests: Constrain rails 6 to ebdaa04

--- gemfiles/rails_6.0.rb
+++ gemfiles/rails_6.0.rb
@@ -3,8 +3,9 @@
 source "https://rubygems.org"
 gemspec path: ".."
 
-# TODO: Use actual version number
-gem "activemodel", github: "rails/rails"
-gem "activerecord", github: "rails/rails"
-gem "activesupport", github: "rails/rails"
+# Rails 6 beta 1 has been released, so you might expect us to use exactly that
+# version here, but it is still in flux, so we may continue using git for a
+# while, maybe until RC 1 is released.
+gem "activerecord", github: "rails/rails", ref: "ebdaa04"
+gem "activesupport", github: "rails/rails", ref: "ebdaa04"
 gem "sqlite3", platforms: :ruby
