2f0c9e27e45dc4e9f0daa610c3db7dca431dc4f1
epaew <epaew.333@gmail.com>
2019-09-25 00:51:36 +0900

    Update adapter_test

--- test/adapter_test.rb
+++ test/adapter_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "authlogic/controller_adapters/rails_adapter"
 
 module Authlogic
   module ControllerAdapters
@@ -17,6 +18,16 @@ module Authlogic
         assert controller.params.equal?(adapter.params)
         assert adapter.respond_to?(:an_arbitrary_method)
         assert_equal "bar", adapter.an_arbitrary_method
+      end
+    end
+
+    class RailsAdapterTest < ActiveSupport::TestCase
+      def test_api_controller
+        controller = MockAPIController.new
+        adapter = Authlogic::ControllerAdapters::RailsAdapter.new(controller)
+
+        assert_equal controller, adapter.controller
+        assert_nil adapter.cookies
       end
     end
   end
