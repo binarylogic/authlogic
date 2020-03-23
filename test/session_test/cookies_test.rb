ba01017360e71471f45ac738d612969eab2c4f8b
Peter Philips <pete@p373.net>
2020-03-23 10:27:03 -0700

    fix whitespace

--- test/session_test/cookies_test.rb
+++ test/session_test/cookies_test.rb
@@ -78,7 +78,7 @@ module SessionTest
         assert_equal "Lax", session.same_site
         session.same_site = "None"
         assert_equal "None", session.same_site
-        
+
         assert_raise(ArgumentError) { UserSession.same_site "foo" }
         assert_raise(ArgumentError) { UserSession.new.same_site "foo" }
       end
