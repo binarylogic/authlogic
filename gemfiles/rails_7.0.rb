84497b92fc273eb423f0351f8c89e6af41d224b9
Dan Laffan <dan.laffan@mac.com>
2021-12-09 17:11:58 +0000

    Updated version of Rails to 7.0.0-rc1

--- gemfiles/rails_7.0.rb
+++ gemfiles/rails_7.0.rb
@@ -5,5 +5,6 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 source "https://rubygems.org"
 gemspec path: ".."
 
-gem "activerecord", "~> 7.0.0.alpha", github: "rails/rails", branch: "main"
-gem "activesupport", "~> 7.0.0.alpha", github: "rails/rails", branch: "main"
+# For some reason, Rails 7.0.0.rc1 is tagged as 7.1.0.alpha
+gem "activerecord", "~> 7.1.0.alpha", github: "rails/rails", branch: "main"
+gem "activesupport", "~> 7.1.0.alpha", github: "rails/rails", branch: "main"
